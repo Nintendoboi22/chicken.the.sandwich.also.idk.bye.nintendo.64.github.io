77df18243b357a6765eaaee299f1db6aff2a4cbc
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-10-01 09:26:13 -0400

    se

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,7 +1,19 @@
 var ROMLIST = [
     
-    {url:"roms/rom1.z64",title:"Game 1"},
-    {url:"roms/rom2.v64",title:"Game 2"},
-    {url:"roms/rom3.v64",title:"Game 3"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/TopGun Demo by Horizon64 (PD).z64",title:"TopGun Demo"},
+    {url:"roms/TR64 Demo by FIres and Icepir8 (PD).z64",title:"Game 3"},
+    {url:"roms/Tristar and Lightforce Quake Intro by Ayatollah & Mike (PD).z64",title:"Tristar and Lightforce Quake Intro"},
+    {url:"roms/TRON Demo (PD).z64",title:"TRON Demo"},
+    {url:"roms/TRSI Intro by Ayatollah(POM '98) (PD).z64",title:"TRSI Intro"},
+    {url:"roms/Twintris by Twinsen (POM '98) (PD).z64",title:"Twintris"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
 
 ];
